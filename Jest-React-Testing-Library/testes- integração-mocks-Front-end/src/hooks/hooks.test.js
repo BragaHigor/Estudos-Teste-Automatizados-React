1d6bd1c83035f8a1822b4c7078d47717e866b0e5
Higor Braga <higorbraga.s@gmail.com>
2023-06-30 17:49:50 -0300

    estudos

---
+++ Jest-React-Testing-Library/testes- integração-mocks-Front-end/src/hooks/hooks.test.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { renderHook } from '@testing-library/react'
+
+test('Hooks', () => {
+
+    const { result } = renderHook(() => {
+
+        const [nome, setNome] = useState('')
+        useEffect(() => {
+
+            setNome('Alice')
+
+        },[])
+
+        return nome
+
+    })
+
+        expect(result.current).toBe('Alice')
+})
---
+++ Jest-React-Testing-Library/testes- integração-mocks-Front-end/src/services/integracoes.test.js
@@ -0,0 +1,54 @@
+import { resolve } from 'styled-jsx/css'
+import api from './api'
+import { buscaTransacoes } from './transacoes'
+
+jest.mock('./api')
+
+const mockTransacao = [
+    {
+        id: 1,
+        transacao: 'Depósito',
+        valor: '100',
+        data: '22/11/2022',
+        mes: 'Novembro'
+    }
+]
+
+const mockRequisicao = (retorno) => {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve({
+                data:retorno
+            })
+        }, 200)
+    })
+}
+
+const mockRequisicaoErro = () => {
+    return new Promise ((_, reject) => {
+        setTimeout(() => {
+            reject()
+        }, 200)
+    })
+}
+
+describe('Requisições para API', () => {
+    test('Deve retornar uma lista de transações', async () => {
+        api.get.mockImplementation(() => mockRequisicao(mockTransacao))
+
+        const trasacoes = await buscaTransacoes()
+        expect(trasacoes).toEqual(mockTransacao)
+        expect(api.get).toHaveBeenCalledWith('/transacoes')
+    })
+
+    test('Deve retornar uma lista vazia quando a requisição falhar', async () => {
+        api.get.mockImplementation(() => mockRequisicaoErro())
+
+        const trasacoes = await buscaTransacoes()
+
+        expect(trasacoes).toEqual([])
+        expect(api.get).toHaveBeenCalledWith('/transacoes')
+    })
+
+
+})
